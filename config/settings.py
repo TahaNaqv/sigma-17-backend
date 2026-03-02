a5c50591e3548651af0cf39aa44588319013f1bb
Taha Abbas <taha.abbas@ideofuzion.com>
2026-03-02 12:36:25 +0500

    cors

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure--xoxe4!yky(^&#d$$w8=m&c$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '0.0.0.0', 'api.sigma17.leadrisks.com'])
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '0.0.0.0', 'api.sigma17.leadrisks.com', "sigma17.netlify.app"])
 
 # Application definition
 
@@ -158,6 +157,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
@@ -169,6 +169,8 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     'http://localhost:3000',
     'http://localhost:5173',
     'https://sigma17.leadrisks.com',
+    "https://www.sigma17.leadrisks.com",
+    "https://sigma17.netlify.app", 
 ])
 
 # Security settings for production
